013560d56754c994c02409ba1f64c88a3e7ec436
nolanplatt <nolanplattgithub@gmail.com>
2017-11-07 19:24:34 -0800

    -Greeter -Everything is awesome

---
+++ src/EverythingIsAwesome.java
@@ -0,0 +1,8 @@
+import javax.swing.JOptionPane;
+
+public class EverythingIsAwesome {
+public static void main(String[] args) {
+	String awsome=JOptionPane.showInputDialog("what is awsome?");
+	JOptionPane.showMessageDialog(null,awsome+" is awsome");
+}
+}
---
+++ src/Greeter.java
@@ -0,0 +1,16 @@
+import javax.swing.JOptionPane;
+
+public class Greeter {
+	public static void main(String[] args) {
+		String name=JOptionPane.showInputDialog("Enter your name");
+		JOptionPane.showMessageDialog(null,"hi "+name);
+	}
+
+
+
+
+
+
+}
+
+
